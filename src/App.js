08654bca258eb12ef8f174f397fda893dc7fcee3
jmscoding100 <joshua.z.mcgee@gmail.com>
2025-05-21 10:33:54 -0500

    commit-4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Routes, Route } from "react-router"
+
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
+import Error from "./components/Error"
 
 const App =()=>{
     return(
         <>
         <Header />
-        <Main />
+        <Routes>
+            <Route path="/" element={<Main />} />
+
+
+            <Route path="*" element={<Error />} />
+        </Routes>
         <Footer />
         </>
     )
